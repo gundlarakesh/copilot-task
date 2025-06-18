8f6f8c01249df76e8dc2b1333bf1a559b15ebd00
Rakesh Gundla <141330065+gundlarakesh@users.noreply.github.com>
2025-06-18 19:00:35 +0530

    Update urls.py

--- it_chatbot_backend/urls.py
+++ it_chatbot_backend/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 def home(request):
     return HttpResponse("IT Chatbot Backend is running.")
 
