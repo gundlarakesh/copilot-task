69195944415dd078e6502148c1fcfc9804ee3f3b
Rakesh Gundla <141330065+gundlarakesh@users.noreply.github.com>
2025-06-18 18:32:37 +0530

    Update settings.py

--- it_chatbot_backend/settings.py
+++ it_chatbot_backend/settings.py
@@ -105,3 +105,6 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
